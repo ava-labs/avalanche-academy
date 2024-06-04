3a28cd1e1cb2c503fe60edff84009f3d9e7be290
Martin Eckardt <martin.eckardt@avalabs.org>
2024-06-04 18:47:56 +0200

    remove output export

--- next.config.js
+++ next.config.js
@@ -18,7 +18,6 @@ const withAnalyzer = createBundleAnalyzer({
 
 /** @type {import('next').NextConfig} */
 const config = {
-  output: "export",
   reactStrictMode: true,
   eslint: {
     // Replaced by root workspace command
