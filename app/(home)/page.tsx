20af47f18f8e7d6fe12ec5893c02da821328589d
Martin Eckardt <martin.eckardt@avalabs.org>
2024-06-07 12:34:56 +0200

    Change placeholder text

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -108,7 +108,7 @@ function Courses(): React.ReactElement {
         <div className="mx-auto w-full lg:mx-0">
           <h2 className="text-center text-3xl font-bold tracking-tight sm:text-4xl">Explore our Courses</h2>
           <p className="mt-2 text-center text-lg leading-8 text-muted-foreground">
-            Learn how to grow your business with our expert advice.
+          We offer fundamental courses are specifically designed for individuals who are new to the Avalanche ecosystem, and advanced courses for those who wish to master the art of configuring, modifying, or even creating entirely new Virtual Machines from scratch.
           </p>
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
