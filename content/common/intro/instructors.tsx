71d3a9d4436262189d74d6064c517c6341db3115
Martin Eckardt <martin.eckardt@avalabs.org>
2024-07-15 12:48:46 +0200

    Team page

---
+++ content/common/intro/instructors.tsx
@@ -0,0 +1,41 @@
+type Instructor = {
+    name: string;
+    title: string;
+    
+    twitter: string;
+    linkedin: string;
+    github: string;
+};
+
+const instructors: Instructor[] = [
+    {
+        name: "Martin Eckardt",
+        title: "Director of Developer Relations",
+
+        twitter: "https://twitter.com/martin_eckardt",
+        linkedin: "https://www.linkedin.com/in/eckardt/",
+        github: "https://github.com/martineckardt",
+    },
+    {
+        name: "Andrea Vargas",
+        title: "Senior Developer Relations Engineer",
+
+        twitter: "https://x.com/Andyvargtz",
+        linkedin: "https://www.linkedin.com/in/andyvargtz/",
+        github: "https://github.com/andyvargtz",
+    },
+    {
+        name: "Ash",
+        title: "Developer Relations Engineer",
+
+        twitter: "https://x.com/ashngmi",
+        linkedin: "https://www.linkedin.com/in/andyvargtz/",
+        github: "https://github.com/ashucoder9",
+    }
+];
+
+function getInstructors(names:string[]) {
+    return instructors.filter((instructor) => names.includes(instructor.name));
+}
+
+export default instructors;
