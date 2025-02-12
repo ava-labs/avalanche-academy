3094334165be9dd3a3dbb815027a7f570a8b1616
Ashutosh Tripathi <39340292+ashucoder9@users.noreply.github.com>
2025-02-12 14:44:31 +0530

    add guides redirects

--- app/[...path]/page.tsx
+++ app/[...path]/page.tsx
@@ -9,6 +9,16 @@ const staticRedirects: RedirectMap = {
   '/course/subnet-architecture': '/academy/multi-chain-architecture',
   '/course/teleporter': '/academy/interchain-messaging',
   '/start': '/',
+  '/guide/install-avalanche-cli': '/guides/install-avalanche-cli',
+  '/guide/deploy-a-dapp-on-c-chain-with-foundry': '/guides/deploy-a-dapp-on-c-chain-with-foundry',
+  '/guide/what-is-a-blockchain': '/guides/what-is-a-blockchain',
+  '/guide/etna-upgrade-motivation': '/guides/etna-upgrade-motivation',
+  '/guide/use-privy-on-l1': '/guides/use-privy-on-l1',
+  '/guide/etna-changes': '/guides/etna-changes',
+  '/guide/l1-economics': '/guides/l1-economics',
+  '/guide/telegram-miniapps-thirdweb': '/guides/telegram-miniapps-thirdweb',
+  '/guide/subnet-vs-l1-validators': '/guides/subnet-vs-l1-validators',
+  '/guide/l1-validator-fee': '/guides/l1-validator-fee',
 };
 
 function getDestinationUrl(currentPath: string): string {
